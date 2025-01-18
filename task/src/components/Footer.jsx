f100fd43db993acfa82e3941a962f0aec6663c72
rakesh baleri <rrbaleri@gmail.com>
2025-01-18 15:34:53 +0530

    images added

--- task/src/components/Footer.jsx
+++ task/src/components/Footer.jsx
@@ -3,132 +3,194 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="md:py-[1.25rem] py-[3.00rem] flex flex-col items-center bg-base-white">
-      <div className="max-w-[80.00rem] sm:gap-[2.00rem] md:px-[1.25rem] gap-[4.00rem] mt-[1.00rem] mx-auto flex w-full flex-col">
-        <div className="sm:px-[1.25rem] px-[2.00rem] flex">
-          <div className="gap-[1.25rem] w-[90%] flex justify-between md:w-full md:flex-col">
-            <div className="gap-[0.88rem] w-[18%] flex flex-col items-start md:w-full">
-              <p className=" text-[0.88rem] font-semibold !text-blue_gray-500">
-                Product
-              </p>
-              <ul className="gap-[0.75rem] flex flex-col items-start">
-                <li>
-                  <a href="#">
-                    <h6 className=" text-[1.00rem] font-semibold !text-gray-600">
-                      Overview
-                    </h6>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <h6 className=" text-[1.00rem] font-semibold !text-gray-600">
-                      Features
-                    </h6>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <h6 className=" text-[1.00rem] font-semibold !text-gray-600">
+      <div className="max-w-[80.00rem] sm:gap-[2.00rem] md:px-[1.25rem] gap-[4.00rem] mt-[1.00rem] mx-auto w-full grid grid-cols-1 gap-[4.00rem]">
+        {/* Row 1 */}
+        <div className="flex flex-col gap-[1.25rem] w-full">
+          {/* Product Section */}
+          <div className="gap-[0.88rem] w-full flex flex-col items-start">
+            <p className="text-[0.88rem] font-semibold !text-blue_gray-500">
+              Product
+            </p>
+            <ul className="gap-[0.75rem] flex flex-col items-start">
+              <li>
+                <a href="#">
+                  <h6 className="text-[1.00rem] font-semibold !text-gray-600">
+                    Overview
+                  </h6>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <h6 className="text-[1.00rem] font-semibold !text-gray-600">
+                    Features
+                  </h6>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <div className="gap-[1.00rem] flex flex-wrap items-center">
+                    <h6 className="text-[1.00rem] font-semibold !text-gray-600">
                       Solutions
                     </h6>
-                    <p className=" text-[0.75rem] font-medium !text-teal-800">
+                    <p className="text-[0.75rem] font-medium !text-teal-800">
                       New
                     </p>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <h6 className=" text-[1.00rem] font-semibold !text-gray-600">
-                      Tutorials
-                    </h6>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <h6 className=" text-[1.00rem] font-semibold !text-gray-600">
-                      Pricing
-                    </h6>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <h6 className=" text-[1.00rem] font-semibold !text-gray-600">
-                      Releases
-                    </h6>
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="gap-[0.88rem] w-[18%] flex flex-col items-start md:w-full">
-              <p className=" text-[0.88rem] font-semibold !text-blue_gray-500">
-                Company
-              </p>
-              <ul className="gap-[0.75rem] flex flex-col items-start">
-                <li>
-                  <a href="#">
-                    <h6 className=" text-[1.00rem] font-semibold !text-gray-600">
-                      About us
-                    </h6>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <h6 className=" text-[1.00rem] font-semibold !text-gray-600">
-                      Careers
-                    </h6>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <h6 className=" text-[1.00rem] font-semibold !text-gray-600">
-                      Press
-                    </h6>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <h6 className=" text-[1.00rem] font-semibold !text-gray-600">
-                      News
-                    </h6>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <h6 className=" text-[1.00rem] font-semibold !text-gray-600">
-                      Media kit
-                    </h6>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <h6 className=" text-[1.00rem] font-semibold !text-gray-600">
-                      Contact
-                    </h6>
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="gap-[0.88rem] w-[18%] flex flex-col items-start md:w-full">
-              <p className=" text-[0.88rem] font-semibold !text-blue_gray-500">
-                Resources
-              </p>
-              <ul className="gap-[0.75rem] flex flex-col items-start">
-                <li>
-                  <a href="#">
-                    <h6 className=" text-[1.00rem] font-semibold !text-gray-600">
-                      Blog
-                    </h6>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <h6 className=" text-[1.00rem] font-semibold !text-gray-600">
-                      Newsletter
-                    </h6>
-                  </a>
-                </li>
-              </ul>
-            </div>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <h6 className="text-[1.00rem] font-semibold !text-gray-600">
+                    Tutorials
+                  </h6>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <h6 className="text-[1.00rem] font-semibold !text-gray-600">
+                    Pricing
+                  </h6>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <h6 className="text-[1.00rem] font-semibold !text-gray-600">
+                    Releases
+                  </h6>
+                </a>
+              </li>
+            </ul>
           </div>
+        </div>
+
+        {/* Row 2 */}
+        <div className="flex flex-col gap-[1.25rem] w-full">
+          {/* Company Section */}
+          <div className="gap-[0.88rem] w-full flex flex-col items-start">
+            <p className="text-[0.88rem] font-semibold !text-blue_gray-500">
+              Company
+            </p>
+            <ul className="gap-[0.75rem] flex flex-col items-start">
+              <li>
+                <a href="#">
+                  <h6 className="text-[1.00rem] font-semibold !text-gray-600">
+                    About us
+                  </h6>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <h6 className="text-[1.00rem] font-semibold !text-gray-600">
+                    Careers
+                  </h6>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <h6 className="text-[1.00rem] font-semibold !text-gray-600">
+                    Press
+                  </h6>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <h6 className="text-[1.00rem] font-semibold !text-gray-600">
+                    News
+                  </h6>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <h6 className="text-[1.00rem] font-semibold !text-gray-600">
+                    Media kit
+                  </h6>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <h6 className="text-[1.00rem] font-semibold !text-gray-600">
+                    Contact
+                  </h6>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/* Row 3 */}
+        <div className="flex flex-col gap-[1.25rem] w-full">
+          {/* Resources Section */}
+          <div className="gap-[0.88rem] w-full flex flex-col items-start">
+            <p className="text-[0.88rem] font-semibold !text-blue_gray-500">
+              Resources
+            </p>
+            <ul className="gap-[0.75rem] flex flex-col items-start justify-center">
+              <li>
+                <a href="#">
+                  <h6 className="text-[1.00rem] font-semibold !text-gray-600">
+                    Blog
+                  </h6>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <h6 className="text-[1.00rem] font-semibold !text-gray-600">
+                    Newsletter
+                  </h6>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <h6 className="text-[1.00rem] font-semibold !text-gray-600">
+                    API
+                  </h6>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <h6 className="text-[1.00rem] font-semibold !text-gray-600">
+                    Docs
+                  </h6>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/* Row 4 */}
+        <div className="flex flex-col gap-[1.25rem] w-full">
+          {/* Legal Section */}
+          <div className="gap-[0.88rem] w-full flex flex-col items-start">
+            <p className="text-[0.88rem] font-semibold !text-blue_gray-500">
+              Legal
+            </p>
+            <ul className="gap-[0.75rem] flex flex-col items-start justify-center">
+              <li>
+                <a href="#">
+                  <h6 className="text-[1.00rem] font-semibold !text-gray-600">
+                    Privacy Policy
+                  </h6>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <h6 className="text-[1.00rem] font-semibold !text-gray-600">
+                    Terms of Use
+                  </h6>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/* Row 5 */}
+        <div className="flex flex-col gap-[1.25rem] w-full">
+          {/* Add any extra section or leave empty */}
+        </div>
+
+        {/* Row 6 */}
+        <div className="flex flex-col gap-[1.25rem] w-full">
+          {/* Add any extra section or leave empty */}
         </div>
       </div>
     </footer>
--- task/src/pages/Beautiful.jsx
+++ task/src/pages/Beautiful.jsx
@@ -6,14 +6,11 @@ const Beautiful = () => {
   return (
     <div>
       <div className="flex flex-col items-center">
-        {/* Container */}
-        <div className="max-w-7xl mx-auto flex flex-col w-full px-8 md:px-5 gap-16">
-          {/* Hero Section */}
-          <div className="flex flex-col items-center px-14 md:px-5 gap-12">
-            {/* New Feature Banner */}
+        <div className="max-w-7xl mx-auto flex flex-col w-full px-8 md:px-5 gap-5">
+          <div className="flex flex-col items-center px-3 md:px-5 gap-12">
             <div className="flex flex-col md:w-full w-[92%] items-center gap-5">
-              <div className="flex items-center justify-center border border-red-600 bg-red-600/10 rounded-lg gap-3 p-2">
-                <p className="border border-red-600 text-red-600 bg-white px-2 py-0.5 text-sm rounded-md">
+              <div className="flex items-center justify-center border border-red-600 bg-red-600/10 rounded-3xl gap-1">
+                <p className="border m-1 border-red-600 text-red-600 bg-white px-2 py-0.5 text-sm  rounded-3xl">
                   New feature
                 </p>
                 <div className="flex items-center gap-2">
@@ -36,27 +33,29 @@ const Beautiful = () => {
             </p>
           </div>
           {/* Buttons */}
-          <div className="flex gap-3">
-            <button className="flex items-center gap-2 px-5 py-3 bg-white border border-gray-200 rounded-full shadow-sm text-gray-800 font-semibold">
+          <div className="flex gap-3 justify-center">
+            <button className="flex items-center gap-2 px-5 py-2 bg-white border border-gray-200 rounded-full shadow-sm text-gray-800 font-semibold">
               <IconButton>
                 <PlayCircleOutlineIcon />
               </IconButton>
               Demo
             </button>
-            <button className="px-5 py-3 bg-red-600 text-white rounded-full font-semibold">
+            <button
+              className="px-7
+             py-2  bg-red-600 text-white rounded-full font-semibold"
+            >
               Sign up
             </button>
           </div>
           {/* Image Section */}
-          <div className="relative w-full h-[35rem] border-8 border-gray-900 rounded-lg overflow-hidden">
+          <div className="relative w-full h-[30rem] border-t-8 border-l-8 border-r-8 border-gray-900 rounded-lg overflow-hidden">
             <img
               className="absolute inset-0 w-full h-full object-cover"
-              src="https://dhws-production.s3.ap-south-1.amazonaws.com/678a8aa8a58033001c177b55/678a8ae81919a0002206d9b6/678a8ae81919a0002206d9bb/appSource/images/img_image_1.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAQODH4IWP4RIAAMUL%2F20250117%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20250117T182912Z&X-Amz-Expires=25200&X-Amz-Signature=212f1cd10d29a7a5acc8296f0ade981748fac824a14acee6a0171df32ed3c97d&X-Amz-SignedHeaders=host"
+              src="https://s3-alpha-sig.figma.com/img/77d8/9fa1/a69b18701253a32f7e394acfc433d49b?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YS-Jtc0-Ve3jaOlOlp-9BpWzlsaMDBADGur869~rLULf71EfaukCJIkRchKg-DjBbHimGJwC3GM-lXYm30LtvL0akS~RqwVXYKhLcTwpqKuZpBNHn4ruGFvsQn-3eTDILJNHJqeu5ZLWTSn1YUSzRuLAKh04dIXPRI16Iss4z8lzFWeSCelmpRBjrIrcnEyuhstpzderuRACZmfRHygbOpj9hN4r9gyUfZ3tNyM8saQGuW8UgF7Yg-XbdeMJWnJ4EaYMyl91Yy3Pb7saIyc5IK1zVcVUljHvUHqOkpwHoUY-LKgk2QQDSm3jKVpYzSuIzCu6-U3qmF~EtGM9nYpS3g__"
               alt="Dashboard Preview"
             />
           </div>
         </div>
-        {/* Footer Divider */}
       </div>
     </div>
   );
